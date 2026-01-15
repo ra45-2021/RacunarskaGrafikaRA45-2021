b0c0fd9cc86daaaebf053c783814c009ee9c564e
Milica Savic <149960261+ra45-2021@users.noreply.github.com>
2026-01-15 12:53:58 +0100

    feat/Initial 3D Project setup

---
+++ Kostur3D/Header/Globals.h
@@ -0,0 +1,13 @@
+#pragma once
+
+extern bool klimaOn;
+extern bool lavorFull;
+
+extern float desiredTemp;
+extern float measuredTemp;
+
+extern float vent;
+extern float water;
+
+extern int SCREEN_WIDTH;
+extern int SCREEN_HEIGHT;
---
+++ Kostur3D/Header/Shader.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <GL/glew.h>
+
+class Shader {
+public:
+    unsigned int ID;
+
+    Shader(const char* vertexPath, const char* fragmentPath);
+    void use();
+    void setFloat(const std::string &name, float value);
+    void setVec3(const std::string& name, float x, float y, float z);
+    void setMat4(const std::string &name, const float* value);
+
+};
